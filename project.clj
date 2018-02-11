4a4d03546475c2810d036531adaa2b54894b0738
paul alvarez <palvarez@greenhouse.io>
2018-02-10 21:53:39 -0500

    the matrix lib was useless

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.4"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
-                 [net.mikera/core.matrix "0.62.0"]])
+                 [org.clojure/math.numeric-tower "0.0.4"]])
