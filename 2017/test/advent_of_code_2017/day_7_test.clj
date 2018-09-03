f0a9cb07e0e327d5330d984673045d0a6001f593
paul alvarez <palvarez@greenhouse.io>
2018-09-02 21:45:27 -0400

    write tests for day-7

--- 2017/test/advent_of_code_2017/day_7_test.clj
+++ 2017/test/advent_of_code_2017/day_7_test.clj
@@ -3,5 +3,12 @@
             [advent-of-code-2017.day-7 :refer :all]))
 
 (deftest find-root-program-test
-  (testing "output"
-    (is (= "tknk" (find-root-program "resources/day-7/input-test")))))
+  (testing "return value"
+    (is (= "tknk" (find-root-program "input-test")))
+    (is (= "dgoocsw" (find-root-program "input")))))
+
+(deftest balance-tower-test
+  (testing "return value"
+    (is (= 60 (:correct-weight (balance-tower "input-test"))))))
+    ;(is (= "dgoocsw" (find-root-program "input")))))
+
