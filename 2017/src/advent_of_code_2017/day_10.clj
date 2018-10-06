6cd4b010ec080449d24dedb03f1a3e02b3861915
paul alvarez <palvarez@greenhouse.io>
2018-10-06 15:54:54 -0400

    finish day 10 part 2

--- 2017/src/advent_of_code_2017/day_10.clj
+++ 2017/src/advent_of_code_2017/day_10.clj
@@ -87,8 +87,11 @@
    (knot-hash numbers 0 0
               (filter #(>= 256 %) lengths)))
   ([numbers position skip [length & lengths]]
+   ;;(prn "numbers" numbers "position" position "skip" skip)
    (if (nil? length)
-     numbers
+     {:numbers numbers
+      :position position
+      :skip skip}
      (let [numbers-cycle (cycle numbers)
            reversed-sublist (->> numbers-cycle
                                  (drop position)
@@ -99,6 +102,7 @@
                                    (take (- (count numbers) length)))
            new-numbers-cycle (cycle (concat reversed-sublist unaffected-sublist))
            new-numbers (->> new-numbers-cycle
+                            ;; hit the back of the list, and then back up until we're immediately before the position
                             (drop (- (count numbers)
                                      position))
                             (take (count numbers)))
@@ -126,13 +130,15 @@
   ;;   v position (drop position) ->> (take (count numbers))
   [1 2 4 3 0] [1 2 4 3 0])
 
+(def lengths-file-contents
+  (->> "resources/day-10/input"
+       slurp
+       clojure.string/trim-newline))
+
 (defn part1 []
-  (let [lengths-file-contents (->> "resources/day-10/input"
-                                   slurp
-                                   clojure.string/trim-newline)
-        lengths (->> (clojure.string/split lengths-file-contents #",")
+  (let [lengths (->> (clojure.string/split lengths-file-contents #",")
                      (map #(Integer/parseInt %)))
-        hash (knot-hash (range 0 256) lengths)]
+        hash (:numbers (knot-hash (range 0 256) lengths))]
     (->> hash
          (take 2)
          (reduce *))))
@@ -155,3 +161,119 @@
               153 154 155 156 157 158 159 160 161 24 23 22 210 118 119 120
               121 0 49 48 47 46 45 44 43 42 41 40 39))
 
+
+;; --- Part Two ---
+
+;; The logic you've constructed forms a single round of the Knot Hash
+;; algorithm; running the full thing requires many of these
+;; rounds. Some input and output processing is also required.
+
+;; First, from now on, your input should be taken not as a list of
+;; numbers, but as a string of bytes instead. Unless otherwise
+;; specified, convert characters to bytes using their ASCII
+;; codes. This will allow you to handle arbitrary ASCII strings, and
+;; it also ensures that your input lengths are never larger than
+;; 255. For example, if you are given 1,2,3, you should convert it to
+;; the ASCII codes for each character: 49,44,50,44,51.
+
+;; Once you have determined the sequence of lengths to use, add the
+;; following lengths to the end of the sequence: 17, 31, 73, 47,
+;; 23. For example, if you are given 1,2,3, your final sequence of
+;; lengths should be 49,44,50,44,51,17,31,73,47,23 (the ASCII codes
+;; from the input string combined with the standard length suffix
+;; values).
+
+;; Second, instead of merely running one round like you did above, run
+;; a total of 64 rounds, using the same length sequence in each
+;; round. The current position and skip size should be preserved
+;; between rounds. For example, if the previous example was your first
+;; round, you would start your second round with the same length
+;; sequence (3, 4, 1, 5, 17, 31, 73, 47, 23, now assuming they came
+;; from ASCII codes and include the suffix), but start with the
+;; previous round's current position (4) and skip size (4).
+
+;; Once the rounds are complete, you will be left with the numbers
+;; from 0 to 255 in some order, called the sparse hash. Your next task
+;; is to reduce these to a list of only 16 numbers called the dense
+;; hash. To do this, use numeric bitwise XOR to combine each
+;; consecutive block of 16 numbers in the sparse hash (there are 16
+;; such blocks in a list of 256 numbers). So, the first element in the
+;; dense hash is the first sixteen elements of the sparse hash XOR'd
+;; together, the second element in the dense hash is the second
+;; sixteen elements of the sparse hash XOR'd together, etc.
+
+;; For example, if the first sixteen elements of your sparse hash are
+;; as shown below, and the XOR operator is ^, you would calculate the
+;; first output number like this:
+
+;; 65 ^ 27 ^ 9 ^ 1 ^ 4 ^ 3 ^ 40 ^ 50 ^ 91 ^ 7 ^ 6 ^ 0 ^ 2 ^ 5 ^ 68 ^ 22 = 64
+
+;; Perform this operation on each of the sixteen blocks of sixteen
+;; numbers in your sparse hash to determine the sixteen numbers in
+;; your dense hash.
+
+;; Finally, the standard way to represent a Knot Hash is as a single
+;; hexadecimal string; the final output is the dense hash in
+;; hexadecimal notation. Because each number in your dense hash will
+;; be between 0 and 255 (inclusive), always represent each number as
+;; two hexadecimal digits (including a leading zero as necessary). So,
+;; if your first three numbers are 64, 7, 255, they correspond to the
+;; hexadecimal numbers 40, 07, ff, and so the first six characters of
+;; the hash would be 4007ff. Because every Knot Hash is sixteen such
+;; numbers, the hexadecimal representation is always 32 hexadecimal
+;; digits (0-f) long.
+
+;; Here are some example hashes:
+
+;; The empty string becomes a2582a3a0e66e6e86e3812dcb672a272.
+;; AoC 2017 becomes 33efeb34ea91902bb2f59c9920caa6cd.
+;; 1,2,3 becomes 3efbe78a8d82f29979031a4aa0b16a9d.
+;; 1,2,4 becomes 63960835bcdc130f0b66d7ff4f6a5a8e.
+
+;; Treating your puzzle input as a string of ASCII characters, what is
+;; the Knot Hash of your puzzle input? Ignore any leading or trailing
+;; whitespace you might encounter.
+
+(def default-extra-bytes [17 31 73 47 23])
+
+(concat (->> "1,2,3"
+             (map byte)
+             (into []))
+        default-extra-bytes)
+
+(defn repeat-knot-hash
+  ([numbers lengths]
+   (repeat-knot-hash (knot-hash numbers lengths) lengths 63))
+  ([{numbers :numbers position :position skip :skip} lengths repeat]
+   (if (= 0 repeat)
+     numbers
+     (recur (knot-hash numbers position skip lengths)
+            lengths
+            (dec repeat)))))
+
+(defn hash-string [string]
+  (let [lengths (concat (->> string
+                             (map byte)
+                             (into []))
+                        default-extra-bytes)
+        ;; _ (prn "lengths" lengths)
+        sparse-hash (repeat-knot-hash (range 0 256) lengths)
+        ;; _ (prn "sparse-hash" sparse-hash)
+        ;; _ (prn "sparse-hash okay?" (->> sparse-hash
+        ;;                                 flatten
+        ;;                                 (into #{})
+        ;;                                 count))
+        ;; dense-hash-intermediate (->> sparse-hash
+        ;;                              (partition 16)
+        ;;                              (map #(apply bit-xor %)))
+        ;; _ (prn "dense-hash-intermediate" dense-hash-intermediate)
+        dense-hash (->> sparse-hash
+                        (partition 16)
+                        (map #(apply bit-xor %))
+                        (map #(format "%2x" %))
+                        (map #(clojure.string/replace % #"^ " "0"))
+                        (apply str))]
+    dense-hash))
+
+(defn part2 []
+  (hash-string lengths-file-contents))
--- 2017/test/advent_of_code_2017/day_10_test.clj
+++ 2017/test/advent_of_code_2017/day_10_test.clj
@@ -4,8 +4,20 @@
 
 (deftest knot-hash-test
   (testing "return value"
-    (is (= '(3 4 2 1 0) (knot-hash (range 0 5) [3 4 1 5])))))
+    (is (= '(3 4 2 1 0) (:numbers (knot-hash (range 0 5) [3 4 1 5]))))))
 
 (deftest part1-test
   (testing "return value"
-    (is (not= 1406 (part1)))))
+    (is (not= 1406 (part1)))
+    (is (= 11413 (part1)))))
+
+(deftest hash-string-test
+  (testing "return value"
+    (is (= "a2582a3a0e66e6e86e3812dcb672a272" (hash-string "")))
+    (is (= "33efeb34ea91902bb2f59c9920caa6cd" (hash-string "AoC 2017")))
+    (is (= "3efbe78a8d82f29979031a4aa0b16a9d" (hash-string "1,2,3")))
+    (is (= "63960835bcdc130f0b66d7ff4f6a5a8e" (hash-string "1,2,4")))))
+
+(deftest part2-test
+  (testing "return value"
+    (is (= "7adfd64c2a03a4968cf708d1b7fd418d" (part2)))))
