d51e2b448dfa5f2933534593b588872a75e3e9a8
paul alvarez <palvarez@greenhouse.io>
2018-11-04 10:54:17 -0500

    finish day 14 part 1, begin part 2

---
+++ 2017/test/advent_of_code_2017/day_14_test.clj
@@ -0,0 +1,7 @@
+(ns advent-of-code-2017.day-14-test
+  (:require [clojure.test :refer :all]
+            [advent-of-code-2017.day-14 :refer :all]))
+
+(deftest count-occupied-test
+  (testing "return value"
+    (is (= 8108 (count-occupied input-test)))))
