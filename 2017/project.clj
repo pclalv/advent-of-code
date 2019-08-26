58524764f31f1fb19a5b12b70d86816b72109ac3
paul alvarez <palvarez@greenhouse.io>
2019-08-26 19:10:48 -0400

    Ensure that we get stacktraces

--- 2017/project.clj
+++ 2017/project.clj
@@ -5,4 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.csv "0.1.4"]
-                 [org.clojure/math.numeric-tower "0.0.4"]])
+                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :jvm-opts ["-XX:MaxJavaStackTraceDepth=-1"
+             "-XX:-OmitStackTraceInFastThrow"])
+
